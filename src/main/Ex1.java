7ade59fdf152b980494970a851d742cb9dc329c1
dasom <lyuds9094@gmail.com>
2024-06-24 16:49:08 +0900

    6.24 3장 수업내용

---
+++ src/main/Ex1.java
@@ -0,0 +1,21 @@
+package main;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = -20;
+		
+		System.out.println(+num1);
+		System.out.println(+num2);
+		
+		System.out.println(-num1);
+		System.out.println(-num2);
+
+		//부호연산자는 값 자체를 바꾸지 않고, 값을 카피해서 사용함
+		System.out.println(num1);
+		System.out.println(num2);
+		
+	}
+
+}
---
+++ src/quiz/Quiz1.java
@@ -0,0 +1,19 @@
+package quiz;
+
+public class Quiz1 {
+
+	public static void main(String[] args) {
+		
+		int num = -10; // -10
+		int result = +num; // -10 +연산자는 부호 그대로 - 연산자는 부호 반대로
+		
+		int num2 = -20; // -20
+		int result2 = -num2; // 20
+		
+		System.out.println(num);
+		System.out.println(result);
+		System.out.println(num2);
+		System.out.println(result2);
+	}
+
+}
